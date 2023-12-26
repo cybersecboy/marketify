86bc27b6697c951849d06cc027b6c22f45320236
Girraj Kushwah <tgirrajindia@gmail.com>
2023-12-26 21:22:42 +0530

    removed bootstrap

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom'
 import { appRouter } from './App'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
